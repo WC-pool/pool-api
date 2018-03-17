d6e07e0ab3696b2ab8dcbd5c691b950165ab563d
Eric Carmona <ericscarmona@yahoo.com>
2018-03-16 19:00:32 -0700

    [rest-server] instantiated server

--- rest-server/src/index.js
+++ rest-server/src/index.js
@@ -1,2 +1,17 @@
 import http from 'http';
 
+import App from './config/express';
+import './config/database';
+
+export const app = App.express;
+
+const server = http.createServer(app);
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, (err) => {
+  if (err) {
+    console.log('Error connecting to server', err);
+  } else {
+    console.log('successfully connected to PORT: ', PORT);
+  }
+})
