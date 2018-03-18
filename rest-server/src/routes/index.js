d48db0a885eade55b3407ae832581ff0b6fc8954
Eric Carmona <ericscarmona@yahoo.com>
2018-03-18 14:08:47 -0700

    [server] rest-server up and running, primitive routes set up

--- rest-server/src/routes/index.js
+++ rest-server/src/routes/index.js
@@ -1,1 +1,11 @@
 import express from 'express';
+
+import authRouter from '../components/auth/authRouter';
+import picksRouter from '../components/picks/picksRouter';
+
+const router = express.Router();
+
+router.use('/auth', authRouter);
+router.use('/picks', picksRouter);
+
+export default router;
